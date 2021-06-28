11131663b224de9bac5cf016b0ca89d022fe6198
Rodrigo Heck <rodrigo-hec@hotmail.com>
2021-06-28 19:31:55 -0300

    Adicionar arquivos de projeto.

---
+++ Funcionarios/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ByteBank.Funcionarios
+{
+    class Funcionario
+    {   
+        // 0 = funcionário
+        // 1 = Diretor
+        // 2 = Designer
+        // N = ...
+        private int _tipo;
+
+
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+
+        public double GetBonificacao()
+        {
+            if (_tipo == 1)
+                return Salario;
+
+            return Salario * 0.10;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+using ByteBank.Funcionarios;
+using System;
+
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Funcionario carlos = new Funcionario(1);
+            carlos.Nome = "Irineu";
+            carlos.Salario = 2000;
+            carlos.CPF = "075.284.959-05";
+
+            Console.WriteLine(carlos.GetBonificacao());
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
